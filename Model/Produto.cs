9f3c84f1b62cf48b6fa20d01d5bb7fc10e189dcd
Bárbara Gabriela <barbaragabi15@hotmail.com>
2021-10-25 12:48:23 -0300

    Adicionar arquivos de projeto.

---
+++ Model/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace InDEVstockWebApp.Model
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public String Modelo { get; set; }
+        public String Cor { get; set; }
+        public int Quantidade { get; set; }
+        public char Tamanho { get; set; }
+        public string image { get; set; }
+    }
+}
