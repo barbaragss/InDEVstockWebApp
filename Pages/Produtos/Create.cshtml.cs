41ddb179fc86dd2dc744705a06bf8a5f95649e9f
b8run <bruno.soarez08@gmail.com>
2021-10-26 15:40:32 -0300

    Page Create

---
+++ Pages/Produtos/Create.cshtml.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using InDEVstockWebApp.Model;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace InDEVstockWebApp.Pages.Produtos
+{
+    public class CreateModel : PageModel
+    {
+        [BindProperty]
+        public Produto produtos { get; set; }
+        //URL servidor
+        string baseUrl = "";
+        public async Task<IActionResult> OnPostAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await client
+                    .PostAsJsonAsync("api/Produtos", produtos);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToPage("./Index");
+                }
+                else
+                {
+                    return RedirectToPage("./Create");
+                }
+            }
+        }
+    }
+}
